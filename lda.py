cf66b705f43100afe5ac3a1a522ce60e03894525
unknown <aghilafalgunana@gmail.com>
2025-01-21 21:44:17 +0530

    LatentDirichletAllocation

---
+++ lda.py
@@ -0,0 +1,20 @@
+import nltk
+nltk.download('punkt')       # For tokenization
+nltk.download('stopwords')   # For stopwords
+nltk.download('wordnet')     # For WordNet lemmatizer
+nltk.download('omw-1.4')     # For WordNet Lemmatizer's additional data
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
+from nltk.stem import PorterStemmer
+import gensim
+import re
+import gensim.corpora as corpora
+import pandas as pd
+
+
+# Read csv file
+data = pd.read_csv('news_articles.csv')
+data.head()
+data.info()
+
